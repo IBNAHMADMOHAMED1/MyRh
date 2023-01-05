53136490d95df79d1c755d7a2d729e9c5bffa782
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-01-05 18:46:54 +0000

    Config

---
+++ api/src/main/java/io/rh/myrh/confg/JwtAuthFilter.java
@@ -0,0 +1,34 @@
+package io.rh.myrh.confg;
+
+import io.rh.myrh.provider.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authorizationHeader.substring(7); // Bearer + space
+        userEmail = jwtProvider.extrcatUsername(jwtToken); // extract email from token
+    }
+}
