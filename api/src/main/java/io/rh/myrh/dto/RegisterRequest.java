39c18c97f1702a021d87cb727bf34c678877e35f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-01-08 20:36:16 +0000

    RegisterRequest

--- api/src/main/java/io/rh/myrh/dto/RegisterRequest.java
+++ api/src/main/java/io/rh/myrh/dto/RegisterRequest.java
@@ -1,2 +1,20 @@
-package io.rh.myrh.dto;public class RegisterRequest {
+package io.rh.myrh.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String password;
+    private String address;
+    private String description;
+    private MultipartFile image;
 }
