7b8e36f97cb625a5c64c1d5ad1ff4a6a1e6bd7e2
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-01-08 20:36:18 +0000

    CloudinaryProvider

--- api/src/main/java/io/rh/myrh/provider/CloudinaryProvider.java
+++ api/src/main/java/io/rh/myrh/provider/CloudinaryProvider.java
@@ -1,2 +1,30 @@
-package io.rh.myrh.provider;public class CloudinaryProvider {
+package io.rh.myrh.provider;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryProvider {
+
+    public String uploadImage(MultipartFile image)  {
+        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "bankconnect",
+                "api_key", "146139492622934",
+                "api_secret", "nF94rFrBb-bJV35__ju_YXC_6AQ"));
+
+        Map uploadResult ;
+        try {
+            uploadResult = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.emptyMap());
+            return uploadResult.get("url").toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
