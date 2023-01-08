73c4215fe827bd289198f92bfd0905a8ec5052d3
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-01-08 20:36:18 +0000

    OfferMapper

--- api/src/main/java/io/rh/myrh/mapper/OfferMapper.java
+++ api/src/main/java/io/rh/myrh/mapper/OfferMapper.java
@@ -1,2 +1,13 @@
-package io.rh.myrh.mapper;public class OfferMapper {
+package io.rh.myrh.mapper;
+
+import io.rh.myrh.dto.OfferDto;
+import io.rh.myrh.entity.Offer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OfferMapper {
+    OfferMapper INSTANCE = Mappers.getMapper(OfferMapper.class);
+
+    OfferDto offerToOfferDto(Offer offer);
 }
