78cd00ee6f0b968b024bc229d12af71cd8655427
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-01-08 20:36:14 +0000

    JwtAuthFilter

--- api/src/main/java/io/rh/myrh/confg/JwtAuthFilter.java
+++ api/src/main/java/io/rh/myrh/confg/JwtAuthFilter.java
@@ -5,22 +5,40 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @Component
-@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
+    private final UserDetailsService userDetailsService;
+    private String jwt;
+    @Autowired
+    @Lazy
+    public JwtAuthFilter(JwtProvider jwtProvider, UserDetailsService userDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.userDetailsService = userDetailsService;
+    }
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
     @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain
-    ) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String userEmail;
@@ -29,6 +47,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authorizationHeader.substring(7); // Bearer + space
-        userEmail = jwtProvider.extrcatUsername(jwtToken); // extract email from token
+        setJwt(jwtToken);
+        jwtProvider.setToken(jwtToken);
+        userEmail = jwtProvider.extrcatUsername(jwtToken);  // extract email from token
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtProvider.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authTonken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authTonken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));  // set details
+                SecurityContextHolder.getContext().setAuthentication(authTonken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
